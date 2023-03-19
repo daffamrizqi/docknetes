9f55fedd088473701549c685b1e94fdd2ea701b1
daffamrizqi <daffamrizqy@gmail.com>
2023-03-19 18:54:44 +0700

    add notes dockerfile instructions: add, command, label

--- pzn/docker-dockerfile/script.sh
+++ pzn/docker-dockerfile/script.sh
@@ -9,6 +9,7 @@ from/Dockerfile
 2. docker build -t daffamrizqi/from from
 - -t = imageName/tag folder
 
+
 # RUN instruction
 # flow 1
 1. RUN mkdir data
@@ -23,3 +24,41 @@ from/Dockerfile
 docker build -t daffamrizqi/run run --progress=plain --no-cache
 
 
+# CMD Instruction
+FROM alpine:3
+
+RUN mkdir "data"
+RUN echo "hello data" > "data/world.txt"
+
+CMD cat "data/world.txt"
+
+docker build -t daffamrizqi/cmd command
+
+- saat dijalankan seharusnya container langsung mati karena hanya menjalankan cat
+- untuk melihat hasil cat bisa dengan docker container logs containerName
+- detail CMD intruction bisa dilihat dengan docker container inspect containerName
+
+
+# LABEL Instruction
+FROM alpine:3
+
+LABEL author="Daffa Muhammad Rizqi"
+LABEL company="Circular Economy Indonesia" position="CTO"
+
+RUN mkdir "data"
+RUN echo "hello data" > "data/world.txt"
+
+CMD cat "data/world.txt"
+
+- detail LABEL instruction yang sudah dibuat bisa dilihat saat docker container inspect containerName
+
+
+# ADD Instruction
+FROM alpine:3
+
+RUN mkdir hello
+ADD text/*.txt hello/
+- add text (sourceFile) -> add all file ends with .txt format inside text directory into 
+a directory called hello created by RUN Instruction -> cat hello.txt inside hello directory
+
+CMD cat "hello/hello.txt"
